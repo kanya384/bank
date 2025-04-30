cb4c85113df28aafa3f1fb952f540d7fb8555a0d
Laurkan Kushu <kanya384@mail.ru>
2025-04-30 09:23:25 +0300

    initial commit

---
+++ accounts/src/main/java/ru/laurkan/bank/accounts/AccountsApp.java
@@ -0,0 +1,11 @@
+package ru.laurkan.bank.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApp.class, args);
+    }
+}
