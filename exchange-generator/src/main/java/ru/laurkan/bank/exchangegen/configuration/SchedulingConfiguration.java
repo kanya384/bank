19511c9f05bb79ae41d756803ff21f90629c47a5
Laurkan Kushu <kanya384@mail.ru>
2025-05-24 19:49:23 +0300

    добавлен exchangegenerator helm chart

--- exchange-generator/src/main/java/ru/laurkan/bank/exchangegen/configuration/SchedulingConfiguration.java
+++ exchange-generator/src/main/java/ru/laurkan/bank/exchangegen/configuration/SchedulingConfiguration.java
@@ -1,7 +1,9 @@
 package ru.laurkan.bank.exchangegen.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@Configuration
 public class SchedulingConfiguration {
 }
