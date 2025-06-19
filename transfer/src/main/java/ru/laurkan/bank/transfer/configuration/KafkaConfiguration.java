b9361f9e5418d64040ec9123a458e866db7eec43
Laurkan Kushu <kanya384@mail.ru>
2025-06-19 21:33:28 +0300

    добавил трейсинг в transfer

--- transfer/src/main/java/ru/laurkan/bank/transfer/configuration/KafkaConfiguration.java
+++ transfer/src/main/java/ru/laurkan/bank/transfer/configuration/KafkaConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin.NewTopics;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import ru.laurkan.bank.events.transfer.TransferEvent;
 
 @Configuration
 public class KafkaConfiguration {
@@ -15,5 +18,12 @@ public class KafkaConfiguration {
                 TopicBuilder.name(OUTPUT_TRANSFER_NOTIFICATION_EVENTS_TOPIC)
                         .build()
         );
+    }
+
+    @Bean
+    public KafkaTemplate<Long, TransferEvent> notificationsTemplate(ProducerFactory<Long, TransferEvent> producerFactory) {
+        KafkaTemplate<Long, TransferEvent> template = new KafkaTemplate<>(producerFactory);
+        template.setObservationEnabled(true);
+        return template;
     }
 }
