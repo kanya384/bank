9fe7f5288add04e2e942db94f3522ea234ff1cb9
Laurkan Kushu <kanya384@mail.ru>
2025-06-19 20:32:25 +0300

    добавлены логирование и трейсинг в front

--- front/src/main/java/ru/laurkan/bank/front/service/impl/TransferServiceImpl.java
+++ front/src/main/java/ru/laurkan/bank/front/service/impl/TransferServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.laurkan.bank.front.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import ru.laurkan.bank.clients.transfer.TransferClient;
@@ -10,6 +11,7 @@ import ru.laurkan.bank.front.dto.transfer.CreateTransactionRequestDTO;
 import ru.laurkan.bank.front.mapper.TransferMapper;
 import ru.laurkan.bank.front.service.TransferService;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class TransferServiceImpl implements TransferService {
@@ -19,12 +21,18 @@ public class TransferServiceImpl implements TransferService {
     @Override
     public Mono<TransactionResponse> selfTransfer(CreateTransactionRequestDTO request) {
         return transferClient
-                .createTransaction(TransactionType.SELF_TRANSFER, transferMapper.map(request));
+                .createTransaction(TransactionType.SELF_TRANSFER, transferMapper.map(request))
+                .doOnError(e -> {
+                    log.error("error create self transfer: {}", e.getMessage());
+                });
     }
 
     @Override
     public Mono<TransactionResponse> transferToOtherUser(CreateTransactionRequestDTO request) {
         return transferClient
-                .createTransaction(TransactionType.TRANSFER_TO_OTHER_USER, transferMapper.map(request));
+                .createTransaction(TransactionType.TRANSFER_TO_OTHER_USER, transferMapper.map(request))
+                .doOnError(e -> {
+                    log.error("error create transfer to other user: {}", e.getMessage());
+                });
     }
 }
