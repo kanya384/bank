60afcb39f72fd38eabcbc0cdbfa90b87ee83aecc
Laurkan Kushu <kanya384@mail.ru>
2025-06-21 22:39:05 +0300

    рефакторинг сервиса oauth2-proxy

--- oauth2-proxy/src/main/java/ru/laurkan/bank/oauth2proxy/service/impl/ProxyServiceImpl.java
+++ oauth2-proxy/src/main/java/ru/laurkan/bank/oauth2proxy/service/impl/ProxyServiceImpl.java
@@ -2,7 +2,6 @@ package ru.laurkan.bank.oauth2proxy.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.webflux.ProxyExchange;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
@@ -10,35 +9,60 @@ import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import ru.laurkan.bank.oauth2proxy.configuration.ProxyConfigurations;
+import ru.laurkan.bank.oauth2proxy.exception.ClientNotDefinedException;
 import ru.laurkan.bank.oauth2proxy.service.ProxyService;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProxyServiceImpl implements ProxyService {
-    @Value("${upstream-uri}")
-    private String upstreamUri;
+    private final ProxyConfigurations proxyConfigurations;
 
     private final ReactiveOAuth2AuthorizedClientManager manager;
     private final ReactiveClientRegistrationRepository clientRegistrationRepository;
 
     @Override
     public Mono<ResponseEntity<byte[]>> doProxy(ProxyExchange<byte[]> proxy) {
-        return getTokenForClient()
+        String clientId = getRootPath(proxy.path());
+        var destinationUri = getDestinationUri(proxy.path(), proxyConfigurations.getClients().values()
+                .stream()
+                .filter(client -> client.clientId().equals(clientId))
+                .findFirst()
+                .orElseThrow(() -> new ClientNotDefinedException("not found client with id = " + clientId))
+                .upstream());
+        log.info("proxying request to: {}", destinationUri);
+        return getTokenForClient(clientId)
                 .flatMap(accessToken -> proxy
-                        .uri(upstreamUri + proxy.path())
+                        .uri(destinationUri)
                         .header("Authorization", "Bearer " + accessToken)
                         .forward()
                 );
     }
 
-    private Mono<String> getTokenForClient() {
+    private Mono<String> getTokenForClient(String clientId) {
         OAuth2AuthorizeRequest req = OAuth2AuthorizeRequest
-                .withClientRegistrationId("default")
+                .withClientRegistrationId(clientId)
                 .principal("N/A")
                 .build();
 
         return manager.authorize(req)
                 .map(client -> client.getAccessToken().getTokenValue());
+    }
+
+    private String getRootPath(String path) {
+        String[] items = path.split("/");
+        if (items.length <= 1) {
+            return "";
+        }
+
+        return items[1];
+    }
+
+    private String getDestinationUri(String incomingPath, String upstream) {
+        return upstream + "/" + Arrays.stream(incomingPath.split("/")).skip(2).collect(Collectors.joining("/"));
     }
 }
